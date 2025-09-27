62aa14eaa302263738e40eaa578d0480d2aedf80
Saveme <voldanchernikov@gmail.com>
2025-09-27 11:03:21 +0300

    Delete Autotests directory

--- Autotests/2 try/1.5/2/index.py
+++
@@ -1,5 +0,0 @@
-          year = int(input("Введите год: "))
-if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    print("Високосный год")
-else:
-    print("Не високосный год")
--- Autotests/python/1.4/3 задание/index.py
+++
@@ -1,7 +0,0 @@
-user_name = "Alice"
-count1 = 10
-_total = 100
-
-print(user_name)
-print(count1)
-print(_total)
